058c75ea543edfa86d98327d896d456ce7c65690
Nick Langan <55940428+W2NJL@users.noreply.github.com>
2024-11-30 20:52:18 -0500

    WNJL mess

---
+++ ios/CarPlayModule.m
@@ -0,0 +1,10 @@
+#import "React/RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(CarPlayModule, NSObject)
+RCT_EXTERN_METHOD(setupCarPlay)
+@end
+//  WNJLRadio
+//
+//  Created by Nick Langan on 11/30/24.
+//
+
